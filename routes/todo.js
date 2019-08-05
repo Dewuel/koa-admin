d4365e03992a4e3b4fbea378f749e90b6955bd3a
Dewuel <jarvanzx@qq.com>
2019-08-05 16:37:07 +0800

    fixed

--- routes/todo.js
+++ routes/todo.js
@@ -4,5 +4,8 @@
  */
 module.exports = (router) => {
   router.prefix('/api')
-  router.post('/add', require('../controllers/todo').addTodo)
+  router.post('/add', require('../controllers/todo').addTodo);
+  router.post('/deleteTodo', require('../controllers/todo').removeTodo);
+  router.post('/getTodos', require('../controllers/todo').getTodos);
+  router.post('/updateTodo', require('../controllers/todo').updateTodos);
 }
